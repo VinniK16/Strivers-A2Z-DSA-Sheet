6d52390e5919b22b0227e09accdfcd53fe0df0f6
VinniK16 <vinni3871@gmail.com>
2025-09-05 21:36:50 +0530

    Update 05_WhileLoops.java

--- Learn the basics/Things to Know/05_WhileLoops.java
+++ Learn the basics/Things to Know/05_WhileLoops.java
@@ -1,13 +1,30 @@
 /*
-Problem: Given two integers, n and m. The task is to check the relation between 
-         n and m. Return "lesser" if n < m,  "equal" if n == m, and "greater" 
-         if n > m.
-Link: https://www.geeksforgeeks.org/problems/java-if-else-decision-making0924/1
+Problem: While loop is another loop like for loop but unlike for loop it only checks for one condition.
+         Here, we will use a while loop and print a number n's table in reverse order.
+         
+Link: https://www.geeksforgeeks.org/problems/while-loop-printtable-java/1
 
 Approach:
-- Use comparison operators (<, ==, >) to check relation between n and m.
-- Return the appropriate string based on the comparison result.
+- Initialize a counter i = 10.
+- Use a while loop until i becomes 0.
+- In each iteration:
+       - Multiply n by i.
+       - Print the result on the same line.
+       - Print a space after each number except the last one.
+       - Decrement i.
 
 Time Complexity: O(1)
 Space Complexity: O(1)
 */
+
+class Solution {
+    public void calculateMultiples(int n) {
+        int i = 10;
+        
+        while(i != 0) {
+            System.out.print(n*i + " ");
+            i--;
+        }
+    }
+}
+
